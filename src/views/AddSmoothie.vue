2578f9df5c36c7cfe16c2784319093448a3795cf
John Knotts <jhnnyk@gmail.com>
2019-11-11 08:45:32 -0700

    saving records to firestore

--- src/views/AddSmoothie.vue
+++ src/views/AddSmoothie.vue
@@ -28,6 +28,9 @@
 </template>
 
 <script>
+import db from '@/firebase/init';
+import slugify from 'slugify';
+
 export default {
   name: 'AddSmoothie',
   data() {
@@ -35,12 +38,35 @@ export default {
       title: null,
       another: null,
       ingredients: [],
-      feedback: null
+      feedback: null,
+      slug: null
     };
   },
   methods: {
     AddSmoothie() {
-      console.log(this.title, this.ingredients);
+      if (this.title) {
+        this.feedback = null;
+
+        // create a slug
+        this.slug = slugify(this.title, {
+          replacement: '-',
+          remove: /[$*_~.()'"!\-:@]/g,
+          lower: true
+        });
+
+        db.collection('smoothies')
+          .add({
+            title: this.title,
+            ingredients: this.ingredients,
+            slug: this.slug
+          })
+          .then(() => {
+            this.$router.push({ name: 'Home' });
+          })
+          .catch(err => console.log(err));
+      } else {
+        this.feedback = 'You must enter a smoothie title';
+      }
     },
     addIng() {
       if (this.another) {
