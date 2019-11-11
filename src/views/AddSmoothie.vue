4a92e534b93fc3739b4a870b363c18672be851c2
John Knotts <jhnnyk@gmail.com>
2019-11-11 08:21:28 -0700

    adding ingredients

--- src/views/AddSmoothie.vue
+++ src/views/AddSmoothie.vue
@@ -8,9 +8,15 @@
       </div>
       <div class="field add-ingredient">
         <label for="add-ingredient">Add an ingredient:</label>
-        <input type="text" name="add-ingredient" />
+        <input
+          type="text"
+          name="add-ingredient"
+          @keydown.tab.prevent="addIng"
+          v-model="another"
+        />
       </div>
       <div class="field center-align">
+        <p class="red-text" v-if="feedback">{{ feedback }}</p>
         <button class="btn pink">Add Smoothie</button>
       </div>
     </form>
@@ -22,12 +28,24 @@ export default {
   name: 'AddSmoothie',
   data() {
     return {
-      title: null
+      title: null,
+      another: null,
+      ingredients: [],
+      feedback: null
     };
   },
   methods: {
     AddSmoothie() {
-      console.log(this.title);
+      console.log(this.title, this.ingredients);
+    },
+    addIng() {
+      if (this.another) {
+        this.ingredients.push(this.another);
+        this.another = null;
+        this.feedback = null;
+      } else {
+        this.feedback = 'You must enter a value to add an ingredient';
+      }
     }
   }
 };
